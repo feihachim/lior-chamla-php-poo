0fb3cb0b0997f8459ab42c12f00942568f21c4c0
Hachim Youssoufa <feihachim@yahoo.fr>
2022-06-18 19:54:46 +0200

    TUTO DONE

--- libraries/database.php
+++ libraries/database.php
@@ -2,6 +2,8 @@
 
 class Database
 {
+    private static $instance = null;
+
     /**
      * Retourne une connexion à la base de données
      *
@@ -9,11 +11,14 @@ class Database
      */
     public static function getPdo(): PDO
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        if (self::$instance === null)
+        {
+            self::$instance = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
 
-        return $pdo;
+        return self::$instance;
     }
 }
